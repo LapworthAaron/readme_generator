e786c778a94b8e5edb6608fb6688de5682c1b081
Aaron Lapworth <aaronlapworth@hotmail.co.uk>
2023-02-17 11:32:38 +0000

    build qs and inquirer and write to file

--- index.js
+++ index.js
@@ -5,16 +5,54 @@ const generateMarkdown = require("./utils/generateMarkdown");
 
 // array of questions for user
 const questions = [
-
+    {type: 'input',
+        message: 'What is the project called?',
+        name: 'title'},
+    {type: 'input',
+        message:"Please fill in the description:",
+        name: 'description'},
+    {type: 'input',
+        message: 'Please add contents(| seperated)',
+        name: 'contents'},
+    {type: 'input',
+        message: 'How do you install it?',
+        name: 'installation'},
+    {type: 'input',
+        message: 'Who do you use it?',
+        name: 'usage'},
+    {type: 'input',
+        message: 'What licence does it use?',
+        name: 'license'},
+    {type: 'input',
+        message: 'Who contributed to this project?',
+        name: 'contributors'},
+    {type: 'input',
+        message: 'What tests do you have?',
+        name: 'tests'},
+    {type: 'input',
+        message: 'What is your github username?',
+        name: 'questions'}
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error(err) : console.log('Success!')
+    );
 }
 
 // function to initialize program
 function init() {
+    let responseObj = {};
+    inquirer
+        .prompt(questions)
+        .then((response) => {
+            responseObj = response
+            console.log(response)
+            writeToFile("readme_test.md",generateMarkdown(responseObj));
+        });
 
+    
 }
 
 // function call to initialize program
